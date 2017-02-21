f8d2eed48962b66d48b1c70ad64a230d18b818a2
boltean <olteanb@hotmail.com>
2017-02-20 20:08:31 -0800

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZenithWebSite.Models;
+using System.Data.Entity;
+
+namespace ZenithWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Events1
+        public ActionResult Index()
+        {
+            //var events = db.Events.Include(p => p.Activity);
+
+
+
+            var sevenDays = (DateTime.Now.AddDays(8 - (int)System.DateTime.Now.DayOfWeek)).ToShortDateString();
+
+            DateTime dateValue = DateTime.Parse(sevenDays);
+
+            var events = from e in db.Events.Include(a => a.Activity)
+                         where e.EventFrom > DateTime.Now && e.EventTo <=dateValue && e.IsActive
+                         orderby e.EventFrom ascending
+                         select e;
+            return View(events.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Migrations/Zenith/Configuration.cs
@@ -0,0 +1,78 @@
+namespace ZenithWebSite.Migrations.Zenith
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using ZenithDataLib.DataContext;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ZenithWebSite.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations\Zenith";
+        }
+
+        protected override void Seed(ZenithWebSite.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("Admin"))
+                roleManager.Create(new IdentityRole("Admin"));
+
+            if (!roleManager.RoleExists("Member"))
+                roleManager.Create(new IdentityRole("Member"));
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            if (userManager.FindByEmail("a@a.a") == null)
+            {
+                var user = new ApplicationUser
+                {
+                    Email = "a@a.a",
+                    UserName = "a",
+                };
+                var result = userManager.Create(user, "P@$$w0rd");
+                if (result.Succeeded)
+                    userManager.AddToRole(userManager.FindByEmail(user.Email).Id, "Admin");
+            }
+
+            if (userManager.FindByEmail("m@m.m") == null)
+            {
+                var user = new ApplicationUser
+                {
+                    Email = "m@m.m",
+                    UserName = "m",
+                };
+                var result = userManager.Create(user, "P@$$w0rd");
+                if (result.Succeeded)
+                    userManager.AddToRole(userManager.FindByEmail(user.Email).Id, "Member");
+            }
+
+
+            context.Activities.AddOrUpdate(
+                a => a.ActivityId, DummyData.getActivities().ToArray());
+            context.SaveChanges();
+
+            context.Events.AddOrUpdate(
+                e => new { e.ActivityDescription, e.EventFrom }, DummyData.getEvents(context).ToArray());
+
+
+        }
+    }
+}
